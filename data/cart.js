cd132f41d77c2c90ae3d3a9852503613a0e995f9
Ye Mun <yemun56@gmail.com>
2024-03-14 14:15:16 +0630

    Generate HTML for order summary

--- data/cart.js
+++ data/cart.js
@@ -1,8 +1,12 @@
 export const cart = [
-    // {
-    //     productName: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    //     quantity:1
-    // }
+    {
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity:2
+    },
+    {
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity:1
+    }
 ];
 
 export function addToCart(productId,quantity){
