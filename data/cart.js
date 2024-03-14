614450bb3978297bb07dbd3ff02524580e611136
Ye Mun <yemun56@gmail.com>
2024-03-14 15:38:42 +0630

    save to localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,16 @@
-export let cart = [
-    {
-        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity:2
-    },
-    {
-        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-        quantity:1
-    }
-];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+if(!cart){
+    cart = [
+        {
+            productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+            quantity:2
+        },
+        {
+            productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+            quantity:1
+        }
+    ];
+}
 
 export function addToCart(productId,quantity){
     let matchingItem;
@@ -24,11 +27,14 @@ export function addToCart(productId,quantity){
                 quantity:quantity
             });
         }
-        
+        saveToLocalStorage();
         //console.log(cartQuantity);
-        console.log(cart);
+        //console.log(cart);
 }
 
+function saveToLocalStorage(){
+    localStorage.setItem('cart',JSON.stringify(cart));
+}
 export function removeFromCart(productId){
     let newCart= [];
         cart.forEach((cartItem)=>{
@@ -37,4 +43,5 @@ export function removeFromCart(productId){
             }
         });
     cart = newCart;
+    saveToLocalStorage();
 }
